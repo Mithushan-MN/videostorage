11c90cca0c68438bf9b6d73703c7248620732bd2
Mithushan-MN <mithushan0099@gmail.com>
2026-05-05 12:50:55 +0530

    server update 8

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const app = express();
 
 // ====================== CORS (Best Version) ======================
 const allowedOrigins = [
-  "https://videostorage.vercel.app",     // Your frontend
+  "https://videostorage.vercel.app",     // Your frontends
   "http://localhost:5173",
   "http://localhost:3000"
 ];
