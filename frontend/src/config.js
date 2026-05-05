c2cdc332fba8821265a977cc69faec817e75681b
Mithushan-MN <mithushan0099@gmail.com>
2026-05-05 12:48:41 +0530

    server update 6

---
+++ frontend/src/config.js
@@ -0,0 +1,15 @@
+// src/config.js
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '[::1]' ||
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+const config = {
+  API_URL: isLocalhost 
+    ? 'http://localhost:5000/api' 
+    : 'https://videostorage-7xwu.vercel.app/api',
+};
+
+export default config;
